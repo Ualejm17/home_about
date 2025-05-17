6fe3e342d2f3cae8f63375b9b797f0309899a5ce
unvka017 <armiguel.03@gmail.com>
2025-05-17 18:02:47 -0400

    about page

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,16 @@
 # configurar un parametro
 from django.urls import path
 from .views import HomePageView
-# el punto es para i
+from .views import AboutPageView
+
 
 urlpatterns = [
 	path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
 ]
 # urlpatterns es una lista de rutas
 # primero creamos template, luego se agrega a settings.py,
 # luego se crea la vista en views.py, y se agrega a urls.py el path
+
+# para crear una vista de un nuevo template, se crea el template
+# se crea la vista en views.py, y se agrega a urls.py el path, luego se agrega eso a urls.py de django_base
